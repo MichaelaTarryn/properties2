856b8de3f948040727b8e76c5127520db9afc675
Michaela <Michaela-13>
2022-07-01 16:32:06 +0200

    firebase stuff

--- public/javascript/code.js
+++ public/javascript/code.js
@@ -1,124 +1,124 @@
-let array = JSON.parse(localStorage.getItem("array"))
-  ? JSON.parse(localStorage.getItem("array"))
-  : [{
-    img:'../images/H1.jpg',
-    button:'For rent',
-    title:"new apartment view",
-    month:'$11,000',
-    bed:2,
-    shower:3,
-    car:1,
-    Sqaure:'4300 SQ FT'
+let array = JSON.parse(localStorage.getItem("array")) ?
+  JSON.parse(localStorage.getItem("array")) :
+  [{
+      img: '../images/H1.jpg',
+      button: 'For rent',
+      title: "new apartment view",
+      month: 3000,
+      bed: 2,
+      shower: 3,
+      car: 1,
+      Sqaure: '4300 SQ FT'
 
-},
-//object 2
-{
-  img:'../images/H2.jpg',
-  button:'For rent',
-  title:"light modern apartment",
-  month:'$12,000',
-  bed:10,
-  shower:6,
-  car:2,
-  Sqaure:'2300 SQ FT'
-},
-//object3
-{
-  img:'../images/H3.jpg',
-  button:'For rent',
-  title:"relaxing apartment",
-  month:'$1,000',
-  bed:4,
-  shower:1,
-  car:1,
-  Sqaure:'7000 SQ FT'
-},
-//object 4
-{
-  img:'../images/H4.jpg',
-  button:'For rent',
-  title:"complex",
-  month:'$5,000',
-  bed:7,
-  shower:2,
-  car:1,
-  Sqaure:'1000 SQ FT'
-},
-//object 5
-{
-  img:'../images/H5.jpg',
-  button:'For rent',
-  title:"comfortable modern apartment",
-  month:'$6,000',
-  bed:9,
-  shower:3,
-  car:1,
-  Sqaure:'4300 SQ FT'
-},
-//object 6
-{
-  img:'../images/H6.jpg',
-  button:'For rent',
-  title:"light modern apartment",
-  month:'$8,000',
-  bed:6,
-  shower:2,
-  car:1,
-  Sqaure:'8300 SQ FT'
-},
-//object 7
-{
-  img:'../images/H7.jpg',
-  button:'For rent',
-  title:"light modern apartment",
-  month:'$19,000',
-  bed:7,
-  shower:1,
-  car:1,
-  Sqaure:'5300 SQ FT'
-},
-//object 8
-{
-  img:'../images/H8.jpg',
-  button:'For rent',
-  title:"relaxing modern apartment",
-  month:'$5,000',
-  bed:6,
-  shower:1,
-  car:1,
-  Sqaure:'1600 SQ FT'
-},
-//object 9
-{
-  img:'../images/H9.jpg',
-  button:'For rent',
-  title:"modern modern apartment",
-  month:'$7,000',
-  bed:5,
-  shower:0,
-  car:1,
-  Sqaure:'1200 SQ FT'
-},
-//object 10
-{
-  img:'../images/H10.jpg',
-  button:'For rent',
-  title:"comfortable modern apartment",
-  month:'$50,000',
-  bed:5,
-  shower:1,
-  car:1,
-  Sqaure:'4300 SQ FT'
-},
-];
+    },
+    //object 2
+    {
+      img: '../images/H2.jpg',
+      button: 'For rent',
+      title: "light modern apartment",
+      month: 1000,
+      bed: 10,
+      shower: 6,
+      car: 2,
+      Sqaure: '2300 SQ FT'
+    },
+    //object3
+    {
+      img: '../images/H3.jpg',
+      button: 'For rent',
+      title: "relaxing apartment",
+      month: 1000,
+      bed: 4,
+      shower: 1,
+      car: 1,
+      Sqaure: '7000 SQ FT'
+    },
+    //object 4
+    {
+      img: '../images/H4.jpg',
+      button: 'For rent',
+      title: "complex",
+      month: 5000,
+      bed: 7,
+      shower: 2,
+      car: 1,
+      Sqaure: '1000 SQ FT'
+    },
+    //object 5
+    {
+      img: '../images/H5.jpg',
+      button: 'For rent',
+      title: "comfortable modern apartment",
+      month: 6000,
+      bed: 9,
+      shower: 3,
+      car: 1,
+      Sqaure: '4300 SQ FT'
+    },
+    //object 6
+    {
+      img: '../images/H6.jpg',
+      button: 'For rent',
+      title: "light modern apartment",
+      month: 8000,
+      bed: 6,
+      shower: 2,
+      car: 1,
+      Sqaure: '8300 SQ FT'
+    },
+    //object 7
+    {
+      img: '../images/H7.jpg',
+      button: 'For rent',
+      title: "light modern apartment",
+      month: 9000,
+      bed: 7,
+      shower: 1,
+      car: 1,
+      Sqaure: '5300 SQ FT'
+    },
+    //object 8
+    {
+      img: '../images/H8.jpg',
+      button: 'For rent',
+      title: "relaxing modern apartment",
+      month: 5000,
+      bed: 6,
+      shower: 1,
+      car: 1,
+      Sqaure: '1600 SQ FT'
+    },
+    //object 9
+    {
+      img: '../images/H9.jpg',
+      button: 'for rent',
+      title: "modern modern apartment",
+      month: 7000,
+      bed: 5,
+      shower: 0,
+      car: 1,
+      Sqaure: '1200 SQ FT'
+    },
+    //object 10
+    {
+      img: '../images/H10.jpg',
+      button: 'for rent',
+      title: "comfortable modern apartment",
+      month: 5000,
+      bed: 5,
+      shower: 1,
+      car: 1,
+      Sqaure: '4300 SQ FT'
+    },
+  ];
 
 //READ
-function readProducts(array)
- {
-let properties = document.getElementById('Properties');
-array.forEach((item, index) => {
+function readProducts(array) {
+  let properties = document.getElementById('Properties');
+  array.forEach((item, index) => {
     properties.innerHTML += `
-    <div class="card" style="width: 18rem;">
+
+    <div id="card" class="card" style="width: 18rem;">
   <img src="${item.img}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">
@@ -154,25 +154,95 @@ ${item.button}
      
 </div>
 `;
-});
+  });
 }
 readProducts(array);
 
-// SORT BY CATEGORY
-function sortCategory() {
-  let category = document.querySelector("#sortCategory").value;
+function loadSearch(n) {
+  document.getElementById('new').innerHTML = ''
+  n.forEach((item, index) => {
+    document.getElementById('new').innerHTML += `
+  <div id="card2" class="card" style="width: 18rem;">
+  <img src="${item.img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${index}">
+${item.button}
+</button>
 
-  if (category == "All") {
-    return readarray(array);
+<!-- Modal -->
+<div class="modal fade" id="exampleModal${index}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"> <p style="color:black;">Welcome to </p>${item.title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body" style="height:30rem">
+        <img src="${item.img}" class="card-img-top" alt="...">
+        <h4 style="color:black;">Price:</h4>${item.month}
+       <h4 style="color:black;"><i class="fa-solid fa-bed"></i></h4>${item.bed}<h4 style="color:black;"><i class="fa-solid fa-shower"></i></h4>
+        ${item.shower} <h4 style="color:black;"><i class="fa-solid fa-car"></i> </h4> ${item.car} <h4 style="color:black;"> <i class="fa-solid fa-ruler-triangle"></i></h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn bg-dark" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </button></h5>
+    <div class="myDIV"><i class="fa-solid fa-bed"> ${item.bed}</i> <i class="fa-solid fa-shower"></i>
+     ${item.shower}  <i class="fa-solid fa-car"></i> ${item.car}  <i class="fa-solid fa-ruler-triangle"></i> ${item.Sqaure}
+     </div>
+     
+</div>
+  `
+
+  })
+}
+document.querySelector('#new').style.display = 'none';
+
+// SORT BY CATEGORY bed
+function sortCategory(bed) {
+  // let bed = document.querySelector("#sortCategory").value;
+  if (bed.target.value === "All") {
+    document.querySelector('#new').style.display = 'none';
+     readProducts(array);
+  }else {
+    document.querySelector('#new').style.display = 'flex';
   }
-
-  let foundarray = array.filter((array) => {
-    return array.category == category;
+  let foundarray = array.filter((x) => {
+    return x.bed == bed.target.value ;
   });
 
-  readProducts(foundarray);
-  console.log(foundarray);
+  console.table(foundarray);
+  if(foundarray.length){
+    loadSearch(foundarray);
+
+  } else {
+    document.getElementById('new').innerHTML = "No matches"
+  }
 }
+
+document.getElementById('sortCategory').addEventListener('change', sortCategory)
+
+
+function sortPrice1(y) {
+  // let bed = document.querySelector("#sortCategory").value;
+
+  if (y.target.value === "All") {
+    return readProducts(array);
+  }
+  let foundarray1 = array.filter((x) => {
+    return x.month <= y.target.value;
+  });
+
+  console.table(foundarray1);
+  loadSearch(foundarray1);
+}
+
+document.getElementById('sortPrice1').addEventListener('change', sortPrice1)
 
 
 function sortName() {
@@ -189,7 +259,7 @@ function sortName() {
   });
   if (direction == "descending") sortedarray.reverse();
   console.log(sortedarray);
-  readProducts(array);
+  loadSearch(array);
 }
 
 // SORT BY PRICE
@@ -197,23 +267,26 @@ function sortName() {
 function sortPrice() {
   let direction = document.querySelector("#sortPrice").value;
 
-  let sortedarray = array.sort((a, b) => a.price - b.price);
-
-  console.log(sortedarray);
-
-  if (direction == "descending") sortedarray.reverse();
-  readProducts(sortedarray);
+  let sortedarray = array.sort((a, b) => a.month - b.month);
+  
+  console.table(sortedarray);
+  
+  if (direction == "descending") {
+    sortedarray.reverse();
+    return loadSearch(sortedarray);
+  }
+  return loadSearch(sortedarray);
 }
 
 // // CREATE
 function createProduct() {
   let title = document.querySelector("#addTitle").value;
-  let bed= document.querySelector("#addBed").value;
+  let bed = document.querySelector("#addBed").value;
   let price = document.querySelector("#addPrice").value;
   let img = document.querySelector("#addImg").value;
-  let button=document.querySelector("#addButton").value;
-  let shower=document.querySelector("#addShower").value;
-  let parking=document.querySelector("#addParking").value;
+  let button = document.querySelector("#addButton").value;
+  let shower = document.querySelector("#addShower").value;
+  let parking = document.querySelector("#addParking").value;
   try {
     if (!title || !price || !img) throw new Error("Please fill in all fields");
     array.push({
@@ -224,7 +297,7 @@ function createProduct() {
       button,
       shower,
       parking,
-    
+
     });
     localStorage.setItem("array", JSON.stringify(array));
     readProducts(array);
@@ -236,36 +309,15 @@ function createProduct() {
 
 console.log(array);
 
-// UPDATE
-// function updateProduct(index) {
-//   let title = document.querySelector(`#editTitle${index}`).value;
-//   let bed = document.querySelector(`#editCategory${index}`).value;
-//   let price = document.querySelector(`#editPrice${index}`).value;
-//   let img = document.querySelector(`#editImg${index}`).value;
-
-//   try {
-//     if (!title || !price || !img) throw new Error("Please fill in all fields");
-//     array[index] = {
-//       title,
-//       bed,
-//       price,
-//       img,
-//       button,
-//     };
-//     localStorage.setItem("array", JSON.stringify(array));
-//     readProducts(array);
-//   } catch (err) {
-//     alert(err);
-//   }
-// }
 
 
-let edit=document.getElementById("load")
 
-function LoadArray(){
-  edit.innerHTML='';
-  array.forEach((item,index)=>{
-    edit.innerHTML+=`
+let edit = document.getElementById("load")
+
+function LoadArray() {
+  edit.innerHTML = '';
+  array.forEach((item, index) => {
+    edit.innerHTML += `
     <div class="row">
 
                 <div class="col-sm">
@@ -291,8 +343,6 @@ function LoadArray(){
                 PRICE
               </div> 
     `
-  }
-  )
+  })
 
 }
-
--- public/javascript/code2.js
+++ public/javascript/code2.js
@@ -5,7 +5,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H1.jpg',
       button: 'For rent',
       title: "new apartment view",
-      month: '$11,000',
+      month: 11000,
       bed: 2,
       shower: 3,
       car: 1,
@@ -17,7 +17,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H2.jpg',
       button: 'For rent',
       title: "light modern apartment",
-      month: '$12,000',
+      month: 12000,
       bed: 10,
       shower: 6,
       car: 2,
@@ -28,7 +28,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H3.jpg',
       button: 'For rent',
       title: "relaxing apartment",
-      month: '$1,000',
+      month: 1000,
       bed: 4,
       shower: 1,
       car: 1,
@@ -39,7 +39,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H4.jpg',
       button: 'For rent',
       title: "complex",
-      month: '$5,000',
+      month: 5000,
       bed: 7,
       shower: 2,
       car: 1,
@@ -50,7 +50,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H5.jpg',
       button: 'For rent',
       title: "comfortable modern apartment",
-      month: '$6,000',
+      month: 6000,
       bed: 9,
       shower: 3,
       car: 1,
@@ -61,7 +61,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H6.jpg',
       button: 'For rent',
       title: "light modern apartment",
-      month: '$8,000',
+      month: 8000,
       bed: 6,
       shower: 2,
       car: 1,
@@ -72,7 +72,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H7.jpg',
       button: 'For rent',
       title: "light modern apartment",
-      month: '$19,000',
+      month: 19000,
       bed: 7,
       shower: 1,
       car: 1,
@@ -83,7 +83,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H8.jpg',
       button: 'For rent',
       title: "relaxing modern apartment",
-      month: '$5,000',
+      month: 5000,
       bed: 6,
       shower: 1,
       car: 1,
@@ -94,7 +94,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H9.jpg',
       button: 'For rent',
       title: "modern modern apartment",
-      month: '$7,000',
+      month: 7000,
       bed: 5,
       shower: 0,
       car: 1,
@@ -105,7 +105,7 @@ let array = JSON.parse(localStorage.getItem("array")) ?
       img: '../images/H10.jpg',
       button: 'For rent',
       title: "comfortable modern apartment",
-      month: '$50,000',
+      month: 50000,
       bed: 5,
       shower: 1,
       car: 1,
@@ -133,32 +133,177 @@ function LoadArray() {
     <!-- icons -->
     <td>
     <!-- Update -->
-    <a type="button" class="btn" data-bs-toggle="modal" data-bs-target="#update${index}"><i class="fa-solid fa-pen-to-square"></i></a>
+    <a type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editProduct${index}"><i class="fa-solid fa-pen-to-square"></i></a>
     
     <!-- Delete -->
-    <a type="button" class="btn" ><i class="fa-solid fa-trash-can"></i></button>
+    <a type="button" class="btn" onclick="deleteProduct(${index})" ><i class="fa-solid fa-trash-can"></i></button>
     </td>
     </tr>
+
+    <div
+    class="modal fade"
+    id="editProduct${index}"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            Edit ${item.title}
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="editTitle${index}" class="form-label">Title</label>
+            <input
+              class="form-control"
+              type="text"
+              name="editTitle${index}"
+              id="editTitle${index}"
+              value="${item.title}"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="editbed${index}" class="form-label">Bed</label>
+            <select
+              class="form-select"
+              name="editbed${index}"
+              id="editBed${index}"
+              value="${item.bed}"
+            >
+            <option value="1"${ item.bed == 1 ? "selected" : "" }>1</option>
+            <option value="2"${ item.bed == 2 ? "selected" : "" }>2</option>
+            <option value="3"${ item.bed == 3 ? "selected" : "" }>3</option>
+            <option value="4"${ item.bed == 4 ? "selected" : "" }>4</option>
+            <option value="5"${ item.bed == 5 ? "selected" : "" }>5</option>
+            <option value="6"${ item.bed == 6 ? "selected" : "" }>6</option>
+            <option value="7"${ item.bed== 7 ? "selected" : "" }>7</option>
+            <option value="8"${ item.bed == 8? "selected" : "" }>8</option>
+            <option value="9"${ item.bed == 9? "selected" : "" }>9</option>
+            <option value="10"${ item.bed == 10 ? "selected" : "" }>10</option>
+
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="editshower${index}" class="form-label">shower</label>
+            <select
+              class="form-select"
+              name="editshower${index}"
+              id="editshower${index}"
+              value="${item.shower}"
+            >
+            <option value="1" ${ item.shower == 1 ? "selected" : "" }>1</option>
+            <option value="2" ${ item.shower == 2 ? "selected" : "" }>2</option>
+            <option value="3" ${ item.shower == 3 ? "selected" : "" }>3</option>
+            <option value="4" ${ item.shower == 4 ? "selected" : "" }>4</option>
+            <option value="4" ${ item.shower == 5 ? "selected" : "" }>5</option>
+            <option value="4" ${ item.shower == 6 ? "selected" : "" }>6</option>
+            <option value="4" ${ item.shower == 7 ? "selected" : "" }>7</option>
+            <option value="4" ${ item.shower == 8 ? "selected" : "" }>8</option>
+            <option value="4" ${ item.shower == 9 ? "selected" : "" }>9</option>
+            <option value="4" ${ item.shower == 10 ? "selected" : "" }>10</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="editsparking${index}" class="form-label">parking</label>
+            <select
+              class="form-select"
+              name="editparking${index}"
+              id="editparking${index}"
+              value="${item.car}"
+            >
+            <option value="1" ${item.car==1?"selected":""}>1</option>
+            <option value="2"${item.car==2?"selected":""}>2</option>
+            <option value="3"${item.car==3?"selected":""}>3</option>
+            <option value="4"${item.car==4?"selected":""}>4</option>
+            <option value="5"${item.car==5?"selected":""}>5</option>
+            <option value="6"${item.car==6?"selected":""}>6</option>
+            <option value="7"${item.car==7?"selected":""}>7</option>
+            <option value="8"${item.car==8?"selected":""}>8</option>
+            <option value="9"${item.car==9?"selected":""}>9</option>
+            <option value="10"${item.car==10?"selected":""}>10</option>
+            </select>
+          </div>
+          <div class="mb-3">
+          <label for="editsbutton${index}" class="form-label">For rent</label>
+          <select
+            class="form-select"
+            name="editrent${index}"
+            id="editrent${index}"
+            value="${item.button}"
+          >
+          <option value="for rent">ren for</option>
+          </select>
+        </div>
+          <div class="mb-3">
+            <label for="editPrice${index}" class="form-label">Monthly Rent</label>
+            <input
+              class="form-control"
+              type="text"
+              name="editmonth${index}"
+              id="editmonth${index}"
+              value="${item.month}"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="editImg${index}" class="form-label">Image URL</label>
+            <input
+              class="form-control"
+              type="text"
+              name="editImg${index}"
+              id="editImg${index}"
+              value="${item.img}"
+            />
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-dismiss="modal"
+            onclick="updateProduct(${index})"
+          >
+            Save changes
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
     `;
   });
 }
 LoadArray();
 
-
+//create
 function createProduct() {
   let title = document.querySelector("#addTitle").value;
   let bed = document.querySelector("#addBed").value;
-  let price = document.querySelector("#addPrice").value;
+  let month = document.querySelector("#addPrice").value;
   let img = document.querySelector("#addImg").value;
   let button = document.querySelector("#addButton").value;
   let shower = document.querySelector("#addShower").value;
   let parking = document.querySelector("#addParking").value;
   try {
-    if (!title || !price || !img) throw new Error("Please fill in all fields");
+    if (!title || !month || !img) throw new Error("Please fill in all fields");
     array.push({
       title,
       bed,
-      price,
+      month,
       img,
       button,
       shower,
@@ -166,21 +311,82 @@ function createProduct() {
 
     });
     localStorage.setItem("array", JSON.stringify(array));
-    // readProducts(array);
+    LoadArray(array);
   } catch (err) {
     alert(err);
   }
 }
 
 // DELETE
-function deleteProduct(position) {
+function deleteProduct(index) {
     let confirmation = confirm(
       "Are you sure you want to delete the selected product?"
     );
-  
+  console.log("deleted please")
     if (confirmation) {
-      array.splice(position, 1);
-      localStorage.setItem("products", JSON.stringify(array));
-    //   readProducts(array);
+      array.splice(index, 1);
     }
+    localStorage.setItem("array", JSON.stringify(array));
+    LoadArray(array);
   }
+
+  // UPDATE
+function updateProduct(id) {
+  let title = document.querySelector(`#editTitle${id}`).value;
+  let bed = document.querySelector(`#editBed${id}`).value;
+  let month = document.querySelector(`#editmonth${id}`).value;
+  let img = document.querySelector(`#editImg${id}`).value;
+  let button = document.querySelector(`#editrent${id}`).value;
+  let shower = document.querySelector(`#editshower${id}`).value;
+  let parking = document.querySelector(`#editparking${id}`).value;
+
+  try {
+    if (!title || !month || !img) throw new Error("Please fill in all fields");
+    array[id] = {
+      title,
+      bed,
+      month,
+      img,
+      shower,
+      parking,
+      button,
+    };
+    localStorage.setItem("array", JSON.stringify(array));
+    // readProducts(array);
+    LoadArray(array);
+  } catch (err) {
+    alert(err);
+  }
+}
+
+
+function sortName() {
+  let direction = document.querySelector("#sortName1").value;
+
+  let sortedarray = array.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+    }
+    if (a.title.toLowerCase() > b.title.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  if (direction == "descending") sortedarray.reverse();
+  console.log(sortedarray);
+  LoadArray(array);
+}
+
+function sortPrice() {
+  let direction = document.querySelector("#sortPrice1").value;
+
+  let sortedarray = array.sort((a, b) => a.month - b.month);
+  
+  console.table(sortedarray);
+  
+  if (direction == "descending") {
+    sortedarray.reverse();
+    return LoadArray(sortedarray);
+  }
+  return LoadArray(sortedarray);
+}
